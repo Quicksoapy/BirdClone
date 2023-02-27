59e14406fceda15f3c89ff49a1ea17367fb1cb94
maya <73587977+Quicksoapy@users.noreply.github.com>
2023-02-27 10:34:39 +0100

    working on register page

---
+++ BirdClone/Pages/Login.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BirdClone.Pages;
+
+public class Login : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ BirdClone/Pages/Register.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BirdClone.Pages;
+
+public class Register : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
