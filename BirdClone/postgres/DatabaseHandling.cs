c5cf0fb6f2ae074ab18c844f76e91ce4ab2d2f96
Quicksoapy <73587977+Quicksoapy@users.noreply.github.com>
2023-03-05 20:39:14 +0100

    DatabaseHandling.cs update

--- BirdClone/postgres/DatabaseHandling.cs
+++ BirdClone/postgres/DatabaseHandling.cs
@@ -4,22 +4,21 @@ namespace BirdClone.postgres;
 
 public class DatabaseHandling
 {
-    public void RegisterHandler(string username, string hashedPassword)
+    public async void RegisterHandler(string username, string hashedPassword)
     {
-        
-        var connection = Globals.GetConnection().Result;
-        var insertQuery =
-            "INSERT INTO accounts (username, password, email, country, created_on, last_login) VALUES ('@name', '@password', '@email', '@country', '@created_on', '@last_login');";
-        using NpgsqlCommand command = new NpgsqlCommand(insertQuery, connection);
-        command.Parameters.AddWithValue("@name", username);
-        command.Parameters.AddWithValue("@password", hashedPassword);
-        command.Parameters.AddWithValue("@email", "");
-        command.Parameters.AddWithValue("@country", "");
-        command.Parameters.AddWithValue("@created_on", DateTime.UtcNow);
-        command.Parameters.AddWithValue("@last_login", DateTime.UtcNow);
-        Console.WriteLine(command.CommandText);
-        var result = command.ExecuteNonQueryAsync();
-        Console.WriteLine(result.Status);
-        
+        await using var cmd = new NpgsqlCommand("INSERT INTO accounts " +
+                                                "(username, password, email, country, created_on, last_login) VALUES " +
+                                                "($1, $2, $3, $4, $5, $6);", Globals.GetConnection().Result){
+            Parameters =
+            {
+                new() {Value = username},
+                new() {Value = hashedPassword},
+                new() {Value = ""},
+                new () {Value = ""},
+                new() {Value = DateTime.UtcNow},
+                new() {Value = DateTime.UtcNow}
+            }
+        };
+        var result = await cmd.ExecuteNonQueryAsync();
     }
 }
